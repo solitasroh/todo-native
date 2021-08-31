38b201d2cdbc5b841eb672221040233bc14f574f
soojang <solitasroh@gmail.com>
2021-08-31 22:27:48 +0900

    eslint 설정

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+  env: {
+    es2021: true,
+    es6: true,
+    node: true,
+    jest: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
+  plugins: ['react', '@typescript-eslint', 'react-hooks', 'prettier'],
+  rules: {
+    'no-use-before-define': 'off',
+    '@typescript-eslint/no-use-before-define': ['error'],
+    'react/jsx-filename-extension': ['warn', { extensions: ['.tsx'] }],
+    'no-shadow': 'off',
+    '@typescript-eslint/no-shadow': ['error'],
+  },
+};
