77d55db45a244eba2b3eebc76f576d6d49f2d683
nahyoungkim <82586783+nahyoungkim@users.noreply.github.com>
2021-09-11 22:40:03 +0900

    TODO List 화면 변경

--- src/Screens/Todo/Temp.tsx
+++ src/Screens/Todo/Temp.tsx
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Text, View } from 'react-native';
 import styled from 'styled-components/native';
 import TodoList from './TodoList';
+import AddButton from './AddButton';
 
 const Container = styled.SafeAreaView`
   flex: 1;
@@ -15,12 +16,46 @@ const HeaderContainer = styled.View`
   justify-content: center;
   align-items: center;
 `;
-const Temp: React.FC = () => {
+
+interface iTodo {
+  index: number;
+  title: string;
+}
+
+const dummytodoList: iTodo[] = [
+  {
+    index: 1,
+    title: 'todo1',
+  },
+  {
+    index: 2,
+    title: 'todo2',
+  },
+];
+
+const Temp: React.FC = () => {  
+  const [addTodo, SetAddTodo] = useState('');
+  const [todoList, SetTodoList] = useState(dummytodoList);
+  const removeTodoList = (index : number) : void => {
+    let list = [...todoList];
+    list.splice(index-1, 1); // 1개만 추출
+    SetTodoList(list);
+  };
+  const addTodoList = (todo : string) : void => {
+    const index = todoList.length + 1;
+    const list = [...todoList,{index: index, title: todo}];
+    console.log(todo);
+    SetTodoList(list);
+    console.log(list);
+    SetAddTodo("");
+    
+  };
   return (
     <Container>
       <Container>
         <HeaderContainer/>
-        <TodoList></TodoList>
+        <TodoList todoList = {todoList} onDelete ={removeTodoList}></TodoList>    
+        <AddButton onPress ={()=>addTodoList(addTodo)}></AddButton>
       </Container>
     </Container>
   );
--- src/Screens/Todo/TodoList.tsx
+++ src/Screens/Todo/TodoList.tsx
@@ -1,102 +1,34 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { FlatList, Text, TextInput, View } from 'react-native';
 import styled from 'styled-components/native';
 import EmptyItem from './EmptyItem';
 import ToDoItem from './TodoItem';
-import AddButton from './AddButton';
-const Container = styled.SafeAreaView`
-  flex: 1;
-  background-color: #fdf6f0;
-  align-items: center;
-  justify-content: center;
-  width: 100%;
-`;
 
-const ItemContainer = styled.View`
-  flex-direction: row;
-  background-color: #eee;
-  margin: 4px 16px;
-  padding: 8px 16px;
-  border-radius: 8px;
-  align-items: center;
-  width: 100%;
-`;
 const List = styled(FlatList)`
   flex: 1;
   width: 100%;  
   padding: 20px 16px;
 `;
-
-interface iTodo {
-  index: number;
-  title: string;
+interface Props {
+  todoList : {
+    index: number,
+    title: string,
+  };
+  onDelete?: ()=>void;
 }
 
-const dummytodoList: iTodo[] = [
-  {
-    index: 1,
-    title: 'todo1',
-  },
-  {
-    index: 2,
-    title: 'todo2',
-  },
-];
-
-const TodoList = () => {
-  // state = {
-  //   addTodo: ""
-  // };
- 
-  // handleTodo = text => { 
-  //   this.setState({ addTodo: text });
-  // };
-  const [addTodo, SetAddTodo] = useState('');
-  const [todoList, SetTodoList] = useState(dummytodoList);
-  const removeTodoList = (index : number) : void => {
-    let list = [...todoList];
-    list.splice(index-1, 1); // 1개만 추출
-    SetTodoList(list);
-  };
-  const addTodoList = (todo : string) : void => {
-    const index = todoList.length + 1;
-    const list = [...todoList,{index: index, title: todo}];
-    console.log(todo);
-    SetTodoList(list);
-    console.log(list);
-    SetAddTodo("");
-    
-  };
-  const renderItem = ({ item }: { item: iTodo }) => (
-    <ToDoItem index={item.index} title={item.title} onDelete={()=>removeTodoList(item.index)}> </ToDoItem>
+const TodoList : React.FC<Props> = () => { 
+  const renderItem = ({ item }: { item: Props }) => (
+    <ToDoItem index={item.todoList.index} title={item.todoList.title} onDelete={()=>item.onDelete}> </ToDoItem>
   );
 
-  return (
-    <Container>
-      <ItemContainer>
-        <TextInput
-          style = {{
-            width : 200,
-            marginTop: 20,
-            marginBottom: 10,
-            paddingHorizontal: 10,
-            height: 40,
-            borderRadius: 10,
-            borderColor: 'gray',
-            borderWidth: 1
-          }}
-          placeholder="할일을 입력하세요"
-          onChangeText={text => SetAddTodo(text)}
-        />
-        <AddButton onPress ={()=>addTodoList(addTodo)}></AddButton>
-      </ItemContainer>      
+  return (  
       <List
         data={todoList}
         ListEmptyComponent={<EmptyItem />}
         keyExtractor={(item) => item.index}
         renderItem={renderItem}
       ></List>
-    </Container>
   );
 };
 
